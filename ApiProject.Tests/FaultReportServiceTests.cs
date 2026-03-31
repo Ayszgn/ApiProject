df248292c583ad55c48dd1ad4845705ee5c3f865
Ayşegül Yagcıoğulları <aysygc.02@gmail.com>
2026-03-31 17:20:46 +0300

    Unit Test bonus tamamlandı.

---
+++ ApiProject.Tests/FaultReportServiceTests.cs
@@ -0,0 +1,41 @@
+﻿using Xunit;
+using ApiProject.Models;
+using ApiProject.Services;
+
+namespace ApiProject.Tests
+{
+    public class FaultReportServiceTests
+    {
+        [Fact]
+        public void UpdatePriority_ChangesPriority_WhenValid()
+        {
+            var service = new FaultReportService();
+
+            var report = new FaultReport
+            {
+                Title = "Test Başlık",
+                Description = "Test Açıklama",
+                Location = "Test Lokasyon",
+                Priority = PriorityLevel.Low
+            };
+
+            service.UpdatePriority(report, PriorityLevel.High);
+
+            Assert.Equal(PriorityLevel.High, report.Priority);
+        }
+        [Fact]
+        public void UpdatePriority_ShouldThrowException_WhenHighToLow()
+        {
+            var service = new FaultReportService();
+            var report = new FaultReport
+            {
+                Title = "Test Başlık",
+                Description = "Test Açıklama",
+                Location = "Test Lokasyon",
+                Priority = PriorityLevel.High
+            };
+            Assert.Throws<Exception>(() =>
+            service.UpdatePriority(report, PriorityLevel.Low));
+        }
+    }
+}
---
+++ ApiProject/Services/FaultReportService.cs
@@ -0,0 +1,16 @@
+﻿using ApiProject.Models;
+
+namespace ApiProject.Services
+{
+    public class FaultReportService
+    {
+        public void UpdatePriority(FaultReport report, PriorityLevel newPriority)
+        {
+            if (report.Priority == PriorityLevel.High && newPriority == PriorityLevel.Low)
+            {
+                throw new Exception("High priority düşürülemez!");
+            }
+            report.Priority = newPriority;
+        }
+    }
+}
